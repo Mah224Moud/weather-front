89a272f869449c0ca71ec37964ab25a06a4993b2
Faycal EL-MAGHOUM <fe874388@MI115-02.iem>
2025-03-06 17:53:35 +0100

    creation des models LAYERS et VILLES

--- src/app/components/arome/arome.component.ts
+++ src/app/components/arome/arome.component.ts
@@ -6,6 +6,8 @@ import { WmsService } from '../../services/wms.service';
 import { Arome } from '../../models/arome';
 import { City } from '../../models/city';
 import { DataService } from '../../data.service';
+import { LAYERS } from '../../models/layers';
+import { VILLES } from '../../models/villes_api';
 
 @Component({
   selector: 'app-arome',
@@ -32,24 +34,7 @@ export class AromeComponent implements OnInit {
   selectedTime: string = '';
   limite : number = 0;
 
-  layers = [
-    { cle: "Température (hauteur spécifiée)", valeur: "TEMPERATURE__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
-    { cle: "Direction et vitesse du vent", valeur: "WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
-    { cle: "Vitesse du vent", valeur: "WIND_SPEED__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
-    { cle: "Rafale de vent", valeur: "WIND_SPEED_GUST__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
-    { cle: "Humidité relative", valeur: "RELATIVE_HUMIDITY__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
-    { cle: "Précipitations sous forme de neige", valeur: "TOTAL_SNOW_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
-    { cle: "Précipitations totales", valeur: "TOTAL_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
-    { cle: "Pression au sol", valeur: "PRESSURE__GROUND_OR_WATER_SURFACE" },
-    { cle: "Nébulosité basse", valeur: "LOW_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
-    { cle: "Nébulosité haute", valeur: "HIGH_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
-    { cle: "Nébulosité moyenne", valeur: "MEDIUM_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
-    { cle: "CAPE (énergie potentielle convective disponible)", valeur: "CONVECTIVE_AVAILABLE_POTENTIAL_ENERGY__GROUND_OR_WATER_SURFACE" },
-    { cle: "Température de brillance", valeur: "BRIGHTNESS_TEMPERATURE__GROUND_OR_WATER_SURFACE" },
-    { cle: "Précipitations sous forme liquide", valeur: "TOTAL_WATER_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
-    { cle: "Taux de précipitations", valeur: "TOTAL_PRECIPITATION_RATE__GROUND_OR_WATER_SURFACE" },
-    { cle: "Altitude géométrique", valeur: "GEOMETRIC_HEIGHT__GROUND_OR_WATER_SURFACE" }
-  ];
+  layers = LAYERS;
   selectedLayer: string = this.layers[0].valeur;
   overlays: L.ImageOverlay[] = []; 
   isLoading: boolean = false; 
@@ -150,70 +135,7 @@ export class AromeComponent implements OnInit {
   }
 
   VilleLoc(city: string): string {
-    const villes: { [key: string]: string } = {
-      "ABBEVILLE": "FRANCE",
-      "LILLE-LESQUIN": "FRANCE",
-      "PTE DE LA HAGUE": "FRANCE",
-      "CAEN-CARPIQUET": "FRANCE",
-      "ROUEN-BOOS": "FRANCE",
-      "REIMS-PRUNAY": "FRANCE",
-      "BREST-GUIPAVAS": "FRANCE",
-      "PLOUMANAC'H": "FRANCE",
-      "RENNES-ST JACQUES": "FRANCE",
-      "ALENCON": "FRANCE",
-      "ORLY": "FRANCE",
-      "TROYES-BARBEREY": "FRANCE",
-      "NANCY-OCHEY": "FRANCE",
-      "STRASBOURG-ENTZHEIM": "FRANCE",
-      "BELLE ILE-LE TALUT": "FRANCE",
-      "NANTES-BOUGUENAIS": "FRANCE",
-      "TOURS": "FRANCE",
-      "BOURGES": "FRANCE",
-      "DIJON-LONGVIC": "FRANCE",
-      "BALE-MULHOUSE": "FRANCE",
-      "PTE DE CHASSIRON": "FRANCE",
-      "POITIERS-BIARD": "FRANCE",
-      "LIMOGES-BELLEGARDE": "FRANCE",
-      "CLERMONT-FD": "FRANCE",
-      "LE PUY-LOUDES": "FRANCE",
-      "LYON-ST EXUPERY": "FRANCE",
-      "BORDEAUX-MERIGNAC": "FRANCE",
-      "GOURDON": "FRANCE",
-      "MILLAU": "FRANCE",
-      "MONTELIMAR": "FRANCE",
-      "EMBRUN": "FRANCE",
-      "MONT-DE-MARSAN": "FRANCE",
-      "TARBES-OSSUN": "FRANCE",
-      "ST GIRONS": "FRANCE",
-      "TOULOUSE-BLAGNAC": "FRANCE",
-      "MONTPELLIER": "FRANCE",
-      "MARIGNANE": "FRANCE",
-      "CAP CEPET": "FRANCE",
-      "NICE": "FRANCE",
-      "PERPIGNAN": "FRANCE",
-      "AJACCIO": "FRANCE",
-      "BASTIA": "FRANCE",
-      "GLORIEUSES": "INDIEN",
-      "JUAN DE NOVA": "INDIEN",
-      "EUROPA": "INDIEN",
-      "TROMELIN": "INDIEN",
-      "GILLOT-AEROPORT": "INDIEN",
-      "PAMANDZI": "MAYOTTE",
-      "LA DESIRADE METEO": "GUADELOUPE",
-      "ST-BARTHELEMY METEO": "GUADELOUPE",
-      "LE RAIZET AERO": "GUADELOUPE",
-      "TRINITE-CARAVEL": "MARTINIQUE",
-      "LAMENTIN-AERO": "MARTINIQUE",
-      "SAINT LAURENT": "GUYANE",
-      "CAYENNE-MATOURY": "GUYANE",
-      "SAINT GEORGES": "GUYANE",
-      "MARIPASOULA": "GUYANE",
-      "DUMONT D'URVILLE": "AUCUN",
-      "KERGUELEN": "AUCUN",
-      "CROZET": "AUCUN",
-      "NOUVELLE AMSTERDAM": "AUCUN",
-      "ST-PIERRE": "AUCUN"
-    };
+    const villes = VILLES;
   
     // Retourne la localisation en fonction de la ville
     return villes[city] || "AUCUN"; // "AUCUN" par défaut si la ville n'est pas trouvée
---
+++ src/app/models/layers.ts
@@ -0,0 +1,18 @@
+export const LAYERS = [
+    { cle: "Température (hauteur spécifiée)", valeur: "TEMPERATURE__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
+    { cle: "Direction et vitesse du vent", valeur: "WIND__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
+    { cle: "Vitesse du vent", valeur: "WIND_SPEED__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
+    { cle: "Rafale de vent", valeur: "WIND_SPEED_GUST__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
+    { cle: "Humidité relative", valeur: "RELATIVE_HUMIDITY__SPECIFIC_HEIGHT_LEVEL_ABOVE_GROUND" },
+    { cle: "Précipitations sous forme de neige", valeur: "TOTAL_SNOW_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
+    { cle: "Précipitations totales", valeur: "TOTAL_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
+    { cle: "Pression au sol", valeur: "PRESSURE__GROUND_OR_WATER_SURFACE" },
+    { cle: "Nébulosité basse", valeur: "LOW_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
+    { cle: "Nébulosité haute", valeur: "HIGH_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
+    { cle: "Nébulosité moyenne", valeur: "MEDIUM_CLOUD_COVER__GROUND_OR_WATER_SURFACE" },
+    { cle: "CAPE (énergie potentielle convective disponible)", valeur: "CONVECTIVE_AVAILABLE_POTENTIAL_ENERGY__GROUND_OR_WATER_SURFACE" },
+    { cle: "Température de brillance", valeur: "BRIGHTNESS_TEMPERATURE__GROUND_OR_WATER_SURFACE" },
+    { cle: "Précipitations sous forme liquide", valeur: "TOTAL_WATER_PRECIPITATION__GROUND_OR_WATER_SURFACE" },
+    { cle: "Taux de précipitations", valeur: "TOTAL_PRECIPITATION_RATE__GROUND_OR_WATER_SURFACE" },
+    { cle: "Altitude géométrique", valeur: "GEOMETRIC_HEIGHT__GROUND_OR_WATER_SURFACE" }
+  ];
